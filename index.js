a2eea38d97d34b52426558a6edd1fa2dbcefd502
j3lte <jwlagendijk@gmail.com>
2014-08-29 19:17:29 +0200

    Adding Grunt and refactoring

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* global next */
+'use strict';
 // Based on https://github.com/neilkod/chucknorrisfacts/blob/master/chuck_norris_facts.py
 
 // REQUIRES
@@ -8,10 +10,11 @@ var request = require('request'),
     fs = require('fs');
 
 // VARIABLES
-    var _facts = [],
-    done = 0, last = 0,
+var _facts = [],
+    done = 0,
+    last = 0,
     outputFilename = 'chucknorris.json',
-    CONCURRENT_REQUESTS = 15;
+    CONCURRENT_REQUESTS = 20;
 
 // INITIALIZE
 getLastPageNumber(getPages);
@@ -23,20 +26,21 @@ getLastPageNumber(getPages);
  */
 
 function getLastPageNumber (callback) {
-  request('http://www.chucknorrisfacts.com/all-chuck-norris-facts?page=1', function(err, resp, body) {
-    if (err)
-      throw err;
+    request('http://www.chucknorrisfacts.com/all-chuck-norris-facts?page=1', function(err, resp, body) {
+        if (err) {
+            throw err;
+        }
 
-    var $ = cheerio.load(body),
-        lastPageNum = parseInt($('a','.pager-last.last').attr('href').split('=')[1]);
+        var $ = cheerio.load(body),
+            lastPageNum = parseInt($('a','.pager-last.last').attr('href').split('=')[1]);
 
-    if (!isNaN(lastPageNum)){
-      console.log('PAGES: ' + lastPageNum);
-      callback(lastPageNum);
-    }
+        if (!isNaN(lastPageNum)){
+            console.log('PAGES: ' + lastPageNum);
+            callback(lastPageNum);
+        }
 
-  });
-};
+    });
+}
 
 /**
  * Get all the pages from 1 to limit
@@ -44,29 +48,30 @@ function getLastPageNumber (callback) {
  * @param {limit} maximum limit of pages, 100 means page 1 to 100
  */
 function getPages (limit) {
-  // create numbered range from 1 to limit
-  var numrange = _.range(1,limit+1);
-  last = limit;
-  // start querying, limiting the concurrent requests
-  async.eachLimit(numrange, CONCURRENT_REQUESTS, getPage, function(err){
-    if (err)
-      return next(err);
-    // output length
-    console.log('Number of entries: ' + _facts.length);
-    if (_facts.length > 0) {
-      // sort array, filter duplicates
-      var facts = {
-        quotes : sortedUnique(_facts)
-      };
-      console.log('Number of unique quotes: ' + facts.quotes.length);
-      writeFile(facts,function () {
-        console.log("JSON saved to " + outputFilename);
-      });
-    }    
-  });
+    // create numbered range from 1 to limit
+    var numrange = _.range(1,limit+1);
+    last = limit;
+    // start querying, limiting the concurrent requests
+    async.eachLimit(numrange, CONCURRENT_REQUESTS, getPage, function (err) {
+        if (err) {
+            return next(err);
+        }
+        // output length
+        console.log('Number of entries: ' + _facts.length);
+        if (_facts.length > 0) {
+            // sort array, filter duplicates
+            var facts = {
+                quotes : sortedUnique(_facts)
+            };
+            console.log('Number of unique quotes: ' + facts.quotes.length);
+            writeFile(facts,function () {
+                console.log('JSON saved to ' + outputFilename);
+            });
+        }
+    });
 }
 
-var sortedUnique = _.compose(_.uniq, function(array) {
+var sortedUnique = _.compose(_.uniq, function (array) {
     return _.sortBy(array, _.identity);
 });
 
@@ -78,27 +83,27 @@ var sortedUnique = _.compose(_.uniq, function(array) {
  * @param {callback} callback function used by async
  */
 function getPage (pagenum, callback) {
-  request('http://www.chucknorrisfacts.com/all-chuck-norris-facts?page='+pagenum, function(err, resp, body) {
-      if (err) {
-        console.log(' error :');
-        console.log(err);
-      }
-              
-      var $ = cheerio.load(body);
-      
-      $(".views-row").each(function () {
-        var $t = $(this),
-            _quote = $t.find('.createYourOwn').text().trim();
-        if (_quote !== ''){
-          _facts.push(_quote);
+    request('http://www.chucknorrisfacts.com/all-chuck-norris-facts?page=' + pagenum, function(err, resp, body) {
+        if (err) {
+            console.log(' error :');
+            console.log(err);
         }
-      });
 
-      done++;
-      console.log('DONE : ' + ((done/last)*100).toFixed(2) + ' % || Processed page: ' + pagenum);
-      callback();
+        var $ = cheerio.load(body);
 
-  });
+        $('.views-row').each(function () {
+            var $t = $(this),
+                    _quote = $t.find('.createYourOwn').text().trim();
+            if (_quote !== ''){
+                _facts.push(_quote);
+            }
+        });
+
+        done++;
+        console.log('DONE : ' + ((done / last) * 100).toFixed(2) + ' % || Processed page: ' + pagenum);
+        callback();
+
+    });
 }
 
 /**
@@ -108,14 +113,14 @@ function getPage (pagenum, callback) {
  * @param {cb} callback function
  */
 function writeFile(obj,cb) {
-  fs.writeFile(outputFilename, JSON.stringify(obj, null, 2), function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      if (typeof cb === 'function'){
-        cb();
-      }
-    }
-  }); 
+    fs.writeFile(outputFilename, JSON.stringify(obj, null, 2), function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (typeof cb === 'function'){
+                cb();
+            }
+        }
+    });
 }
 
